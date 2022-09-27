2f289e06a1f207dea52f6b836f4e92fd5bacf62c
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-27 13:00:32 +0800

    feat: add CreateBabyName v1

---
+++ naming/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"math/rand"
+)
+
+var names = []string{"Haoran", "Derek", "Iris", "Mike", "Tim", "Jenifer", "Ella", "Jianguo"}
+
+func CreateBabyName() string {
+	index := rand.Intn(len(names) - 1)
+	return names[index]
+}
