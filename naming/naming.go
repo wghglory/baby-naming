b34dd89b07f28bc872e2339086e0ff52b164f833
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-27 21:40:03 +0800

    feat: refactor naming func

--- naming/naming.go
+++ naming/naming.go
@@ -3,16 +3,26 @@ package naming
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 var maleNames = []string{"Haoran", "Derek", "Mike", "Tim", "Jianguo"}
 var femaleNames = []string{"Iris", "Jenifer", "Ella"}
 
-func CreateBabyName(male bool, minLen int) (string, error) {
-	rand.Seed(time.Now().UnixNano())
+// return a baby name by gender and min length
+func CreateBabyName(male bool, minLen uint) (string, error) {
+	names := getBabyNames(male)
 
-	var result string
+	for i := 0; i < len(names); i++ {
+		if uint(len(names[i])) >= minLen {
+			return names[i], nil
+		}
+	}
+
+	return "", fmt.Errorf("no baby found with min length %v", minLen)
+}
+
+// get baby names randomly (shuffled)
+func getBabyNames(male bool) []string {
 	var names []string
 
 	if male {
@@ -21,18 +31,9 @@ func CreateBabyName(male bool, minLen int) (string, error) {
 		names = femaleNames
 	}
 
-	nameLen := len(names)
-
-	rand.Shuffle(nameLen, func(i, j int) {
+	rand.Shuffle(len(names), func(i, j int) {
 		names[i], names[j] = names[j], names[i]
 	})
 
-	for i := 0; i < nameLen; i++ {
-		if len(names[i]) >= minLen {
-			result = names[i]
-			return result, nil
-		}
-	}
-
-	return "", fmt.Errorf("no baby found with min length %v", minLen)
+	return names
 }
